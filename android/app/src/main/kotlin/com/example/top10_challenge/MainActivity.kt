9614c3cb5e58668288e9bff4aeb237e70f7920b9
Mathieu <mathieu.raholijaona@misyapp.com>
2025-08-11 17:21:43 +0200

    Mettre à jour les icônes et configuration d'application
    - Nouveaux icônes d'application pour toutes les plateformes
    - Mise à jour des configurations Android, iOS, web, macOS et Windows
    - Ajout des dépendances nécessaires dans pubspec.yaml
    - Configuration des écrans de lancement et splash screens

--- android/app/src/main/kotlin/com/example/top10_challenge/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.top10_challenge
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/lazypanda/top10_challenge/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lazypanda.top10_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
