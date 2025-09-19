2c54dffba963645608252e10f6fc1be7f6062a59
Mathieu <mathieu.raholijaona@misyapp.com>
2025-09-19 21:49:36 +0200

    Modification du nombre de pubs nécessaires pour les bonus

--- lib/config/money_time_config.dart
+++ lib/config/money_time_config.dart
@@ -2,9 +2,9 @@
 class MoneyTimeConfig {
   /// Mapping from duration in minutes to number of ads required
   static const Map<int, int> durationToAds = {
-    15: 4,  // 15 minutes = 2 ads
-    30: 6,  // 30 minutes = 3 ads (default)
-    60: 8,  // 60 minutes = 5 ads
+    15: 1,  // 15 minutes = 1 ad
+    30: 2,  // 30 minutes = 2 ads (default)
+    60: 3,  // 60 minutes = 3 ads
   };
   
   /// List of available durations in minutes
